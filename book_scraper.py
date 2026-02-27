218968599b5457fec2ec1fb5374b66b717546ee2
NirmalaJyothiGadela <158562750+NirmalaJyothiGadela@users.noreply.github.com>
2026-02-27 23:56:37 +0530

    Added comments for better code understanding

--- book_scraper.py
+++ book_scraper.py
@@ -4,18 +4,19 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-
+# Base URL of the website ({} will be replaced with page numbers)
 base_url = "http://books.toscrape.com/catalogue/page-{}.html"
 
 all_books = []
 
-# site has 50 pages
+# The website contains 50 pages of books
 for page in range(1, 51):
     
     url = base_url.format(page)
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-
+    
+    # Find all book containers on the page
     books = soup.find_all("article", class_="product_pod")
 
     for book in books:
@@ -26,8 +27,7 @@ for page in range(1, 51):
 
     print(f"Page {page} scraped")
 
-
-# creating csv file
+# Create and write data into CSV file
 with open("books_dataset.csv", "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
     writer.writerow(["Title","Price"])
@@ -35,3 +35,4 @@ with open("books_dataset.csv", "w", newline="", encoding="utf-8") as f:
 
 
 print(f"Done! Saved {len(all_books)} books to CSV")
+
